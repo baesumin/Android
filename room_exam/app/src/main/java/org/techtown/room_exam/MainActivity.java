473c785e97ad3334ade8bdef522e389ded42f0dd
SM <52240990+baesumin@users.noreply.github.com>
2020-12-30 12:28:47 +0900

    room_Exam_Java

---
+++ room_exam/app/src/main/java/org/techtown/room_exam/MainActivity.java
@@ -0,0 +1,39 @@
+package org.techtown.room_exam;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText mTodoEditText;
+    private TextView mResultTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTodoEditText = findViewById(R.id.todo_edit);
+        mResultTextView = findViewById(R.id.result_text);
+
+        final AppDatabase db = Room.databaseBuilder(this, AppDatabase.class, "todo-db")
+                .allowMainThreadQueries()
+                .build();
+
+        mResultTextView.setText(db.todoDao().getAll().toString());
+
+        findViewById(R.id.add_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                db.todoDao().insert(new Todo(mTodoEditText.getText().toString()));
+                mResultTextView.setText(db.todoDao().getAll().toString());
+            }
+        });
+    }
+
+
+}
