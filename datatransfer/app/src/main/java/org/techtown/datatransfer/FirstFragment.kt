697cf3dc980b87b2c3ede6c43d2829258b8a20bf
SM <52240990+baesumin@users.noreply.github.com>
2021-01-11 21:58:18 +0900

    fragment

---
+++ datatransfer/app/src/main/java/org/techtown/datatransfer/FirstFragment.kt
@@ -0,0 +1,13 @@
+package org.techtown.datatransfer
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class FirstFragment : Fragment(R.layout.fragment_first) {
+
+
+
+}
