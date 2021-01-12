f4441f6dafff58d3d328ca81ff20dc6fa77daf78
SM <52240990+baesumin@users.noreply.github.com>
2021-01-12 11:58:45 +0900

    fragement_datatransfer

--- datatransfer/app/src/main/java/org/techtown/datatransfer/FirstFragment.kt
+++ datatransfer/app/src/main/java/org/techtown/datatransfer/FirstFragment.kt
@@ -1,23 +1,24 @@
 package org.techtown.datatransfer
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
+import androidx.core.os.bundleOf
+import androidx.fragment.app.*
 import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_first.*
 
 class FirstFragment : Fragment(R.layout.fragment_first) {
-    val mainViewModel by activityViewModels<MainViewModel>()
+    //val mainViewModel by activityViewModels<MainViewModel>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         button.setOnClickListener {
-            mainViewModel.data = "hello"
+            //mainViewModel.data = "hello"
+            setFragmentResult("firstToSecond", bundleOf("data" to "hello"))
+
             findNavController().navigate(R.id.action_firstFragment_to_secondFragment)
         }
     }
--- datatransfer/app/src/main/java/org/techtown/datatransfer/SecondFragment.kt
+++ datatransfer/app/src/main/java/org/techtown/datatransfer/SecondFragment.kt
@@ -7,16 +7,22 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.setFragmentResultListener
 import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_first.*
 
 class SecondFragment : Fragment(R.layout.fragment_second) {
-    val mainViewModel by activityViewModels<MainViewModel>()
+    //val mainViewModel by activityViewModels<MainViewModel>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        Toast.makeText(requireContext(), mainViewModel.data, Toast.LENGTH_LONG).show()
+        setFragmentResultListener("firstToSecond"){resultKey, bundle ->
+            val data = bundle.getString("data","")
+            Toast.makeText(requireContext(), data, Toast.LENGTH_LONG).show()
+        }
+
+        //Toast.makeText(requireContext(), mainViewModel.data, Toast.LENGTH_LONG).show()
 
         button.setOnClickListener {
             findNavController().navigate(R.id.action_secondFragment_to_firstFragment)
