0412c5f0d63e05ba89c4e5958217fe9b3766b3f9
baesumin <qotnals12345@naver.com>
2021-08-27 16:57:38 +0900

    update

--- MakeItRain/app/src/main/java/org/techtown/makeitrain/MainActivity.java
+++ MakeItRain/app/src/main/java/org/techtown/makeitrain/MainActivity.java
@@ -2,13 +2,45 @@ package org.techtown.makeitrain;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.icu.text.NumberFormat;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+    private Button makeItRain;
+    private Button showInfo;
+    private TextView moneyValue;
+    private int moneyCounter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        makeItRain = findViewById(R.id.buttonMakeItRain);
+        moneyValue = findViewById(R.id.moneyValue);
+
+        //makeItRain.setOnClickListener(v -> Log.d("MainActivity", "onClick: Make it Rain!"));
+    }
+
+    public void showMoney(View view) {
+        NumberFormat numberFormat = NumberFormat.getCurrencyInstance();
+
+        moneyCounter += 1000;
+        moneyValue.setText(String.valueOf(numberFormat.format(moneyCounter)));
+        Log.d("MIR", "onClick: " + moneyCounter);
+    }
+
+    public void showInfo(View view) {
+        //Toast.makeText(MainActivity.this, R.string.app_info, Toast.LENGTH_SHORT).show();
+
+        Snackbar.make(moneyValue,R.string.app_info,Snackbar.LENGTH_LONG).setAction("More", view1 ->{
+            Log.d("Snack", "showInfo: Snackbar More");
+        }).show();
     }
 }
