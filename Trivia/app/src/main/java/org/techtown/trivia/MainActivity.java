df36570451514fd28c0d0a84f9fbd2d839aabeb5
baesumin <qotnals12345@naver.com>
2021-09-14 21:20:19 +0900

    anim

--- Trivia/app/src/main/java/org/techtown/trivia/MainActivity.java
+++ Trivia/app/src/main/java/org/techtown/trivia/MainActivity.java
@@ -1,23 +1,83 @@
 package org.techtown.trivia;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import org.techtown.trivia.data.Repository;
+import org.techtown.trivia.databinding.ActivityMainBinding;
 import org.techtown.trivia.model.Question;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+  private ActivityMainBinding binding;
+  private int currentQuestionIndex = 0;
+  List<Question> questionList;
 
-        List<Question> questions = new Repository().getQuestions(questionArrayList ->
-                Log.d("Main", "onCreate: "+questionArrayList));
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+
+    questionList = new Repository().getQuestions(questionArrayList -> {
+              binding.questionTextview.setText(questionArrayList.get(currentQuestionIndex).getAnswer());
+
+              updateCounter(questionArrayList);
+            }
+
+    );
+
+    binding.buttonNext.setOnClickListener(v -> {
+      currentQuestionIndex = (currentQuestionIndex + 1) % questionList.size();
+      updateQuestion();
+    });
+    binding.buttonTrue.setOnClickListener(v -> {
+      checkAnswer(true);
+      updateQuestion();
+    });
+    binding.buttonFalse.setOnClickListener(v -> {
+      checkAnswer(false);
+      updateQuestion();
+    });
+  }
+
+  private void checkAnswer(boolean userChoseCorrect) {
+    boolean answer = questionList.get(currentQuestionIndex).isAnswerTrue();
+    int snackMessageId = 0;
+    if (userChoseCorrect == answer) {
+      snackMessageId = R.string.correct_answer;
+    } else {
+      snackMessageId = R.string.incorrect;
+      shakeAnimation();
     }
+    Snackbar.make(binding.cardView, snackMessageId, Snackbar.LENGTH_SHORT).show();
+  }
+
+  private void updateCounter(ArrayList<Question> questionArrayList) {
+    binding.textViewOutOf.setText("Question:" + currentQuestionIndex + "/" + questionArrayList.size());
+  }
+
+  private void updateQuestion() {
+    String question = questionList.get(currentQuestionIndex).getAnswer();
+    binding.questionTextview.setText(question);
+    updateCounter((ArrayList<Question>) questionList);
+  }
+
+  private void shakeAnimation(){
+    Animation shake = AnimationUtils.loadAnimation(MainActivity.this,R.anim.shake_animation);
+    binding.cardView.setAnimation(shake);
+  }
+
 }
--- Trivia/app/src/main/java/org/techtown/trivia/model/Question.java
+++ Trivia/app/src/main/java/org/techtown/trivia/model/Question.java
@@ -1,38 +1,38 @@
 package org.techtown.trivia.model;
 
 public class Question {
-    private String answer;
-    private boolean answerTrue;
+  private String answer;
+  private boolean answerTrue;
 
-    public Question() {
-    }
+  public Question() {
+  }
 
-    public Question(String answer, boolean answerTrue) {
-        this.answer = answer;
-        this.answerTrue = answerTrue;
-    }
+  public Question(String answer, boolean answerTrue) {
+    this.answer = answer;
+    this.answerTrue = answerTrue;
+  }
 
-    public String getAnswer() {
-        return answer;
-    }
+  public String getAnswer() {
+    return answer;
+  }
 
-    public void setAnswer(String answer) {
-        this.answer = answer;
-    }
+  public void setAnswer(String answer) {
+    this.answer = answer;
+  }
 
-    public boolean isAnswerTrue() {
-        return answerTrue;
-    }
+  public boolean isAnswerTrue() {
+    return answerTrue;
+  }
 
-    public void setAnswerTrue(boolean answerTrue) {
-        this.answerTrue = answerTrue;
-    }
+  public void setAnswerTrue(boolean answerTrue) {
+    this.answerTrue = answerTrue;
+  }
 
-    @Override
-    public String toString() {
-        return "Question{" +
-                "answer='" + answer + '\'' +
-                ", answerTrue=" + answerTrue +
-                '}';
-    }
+  @Override
+  public String toString() {
+    return "Question{" +
+            "answer='" + answer + '\'' +
+            ", answerTrue=" + answerTrue +
+            '}';
+  }
 }
