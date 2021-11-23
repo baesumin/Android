747e1e3df5c1f5465945aab44ff0acba4253342d
baesumin <qotnals12345@naver.com>
2021-11-23 18:38:02 +0900

    room

---
+++ RoomDemo/app/src/main/java/org/techtown/roomdemo/db/SubscriberDAO.kt
@@ -0,0 +1,22 @@
+package org.techtown.roomdemo.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface SubscriberDAO {
+  @Insert
+  suspend fun insertSubscriber(subscriber: Subscriber): Long
+
+  @Update
+  suspend fun updateSubscriber(subscriber: Subscriber)
+
+  @Delete
+  suspend fun deleteSubscriber(subscriber: Subscriber)
+
+  @Query("DELETE FROM subscriber_data_table")
+  suspend fun deleteAll()
+
+  @Query("SELECT * FROM subscriber_data_table")
+  fun getAllSubscribers():LiveData<List<Subscriber>>
+}
