ead23f24dfc63bc5fc50f559749209b8b58d1fbd
SM <52240990+baesumin@users.noreply.github.com>
2024-06-18 17:00:31 +0900

    숫자세기 앱 완료

--- 2024+/chapter2(counter)/app/src/main/java/com/example/chapter2counter/MainActivity.kt
+++ 2024+/chapter2(counter)/app/src/main/java/com/example/chapter2counter/MainActivity.kt
@@ -1,20 +1,32 @@
 package com.example.chapter2counter
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val numberTextView = findViewById<TextView>(R.id.numberTextView)
+        val resetButton = findViewById<Button>(R.id.resetButton)
+        val plusButton = findViewById<Button>(R.id.plusButton)
+
+        var number = 0
+
+        resetButton.setOnClickListener {
+            number = 0
+            numberTextView.text = number.toString()
+            Log.d("onClick","리셋 버튼 클릭")
+        }
+
+        plusButton.setOnClickListener {
+            number++
+            numberTextView.text = number.toString()
+            Log.d("onClick","플러스 버튼 클릭")
         }
     }
 }
